82c122055d45c7c69f7029463ad29b3edfbecd38
Stefan Klinger <stefanklinger@yahoo.com>
2013-05-27 21:47:02 +0200

    fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -94,4 +95,20 @@ describe User do
     end
     it { should_not be_valid }
   end
+  
+  describe "return value of authenticate method" do
+  before { @user.save }
+  let(:found_user) { User.find_by_email( @user.email) }
+
+  describe "with valid password" do
+    it { should eq found_user.authenticate(@user.password) }
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not eq user_for_invalid_password }
+    specify { expect(user_for_invalid_password).to be_false }
+  end
+end
 end
