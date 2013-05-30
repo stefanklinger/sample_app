353a2f2b7b20c5c83de2d31cad4fa8c65884872c
Stefan Klinger <stefanklinger@yahoo.com>
2013-05-30 17:26:16 +0200

    Remember me token

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,14 @@
 
 class User < ActiveRecord::Base
   #before_save { self.email = email.downcase }
+  #before_save { |user| user.email = email.downcase }
+
   before_save { email.downcase! }
+  before_save :create_remember_token
+  
   attr_accessible :email, :name, :password, :password_confirmation
   validates :name, presence: true, length: { maximum: 50 }
+  
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
   validates :email, presence:   true,
@@ -22,4 +27,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+  
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
