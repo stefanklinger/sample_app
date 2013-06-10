725813428018bfea7de305f609a5f5047b9929a2
Stefan Klinger <stefanklinger@yahoo.com>
2013-06-10 20:48:37 +0200

    Micropost model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,19 @@
 #
 
 class User < ActiveRecord::Base
-  #before_save { self.email = email.downcase }
-  #before_save { |user| user.email = email.downcase }
-
   before_save { email.downcase! }
   before_save :create_remember_token
   
   attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   validates :name, presence: true, length: { maximum: 50 }
   
-  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :microposts
+  
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
   
