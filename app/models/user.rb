1e49bfad835cd8adda3153c362226e6c6d12bea3
Stefan Klinger <stefanklinger@yahoo.com>
2013-06-17 21:36:52 +0200

    Microposts feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
 
     def create_remember_token
