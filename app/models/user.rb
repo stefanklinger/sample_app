0ef9a9d02808d431d3696faf89af229191f5c1da
Stefan Klinger <stefanklinger@yahoo.com>
2013-07-01 21:05:08 +0200

    Reverse relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,17 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+           class_name:  "Relationship",
+           dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
   
