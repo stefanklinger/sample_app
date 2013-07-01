fec404eb9c908f86b14ff487acdfa314e44b513d
Stefan Klinger <stefanklinger@yahoo.com>
2013-07-01 20:59:51 +0200

    Testing following

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
@@ -34,7 +35,19 @@ class User < ActiveRecord::Base
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
   end
-  
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   private
 
     def create_remember_token
